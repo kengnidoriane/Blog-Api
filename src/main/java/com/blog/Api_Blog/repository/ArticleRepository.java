454030add1ac815df5061c64f70e184de3812694
kengnidoriane <dorianekengni@gmail.com>
2025-06-23 15:18:34 +0200

    feat: add article and comment epository

--- src/main/java/com/blog/Api_Blog/repository/ArticleRepository.java
+++ src/main/java/com/blog/Api_Blog/repository/ArticleRepository.java
@@ -1,4 +1,11 @@
 package com.blog.Api_Blog.repository;
 
-public class ArticleRepository {
+
+import com.blog.Api_Blog.model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
 }
--- src/main/java/com/blog/Api_Blog/repository/CommentRepository.java
+++ src/main/java/com/blog/Api_Blog/repository/CommentRepository.java
@@ -1,4 +1,10 @@
 package com.blog.Api_Blog.repository;
 
-public class CommentRepository {
+import com.blog.Api_Blog.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
