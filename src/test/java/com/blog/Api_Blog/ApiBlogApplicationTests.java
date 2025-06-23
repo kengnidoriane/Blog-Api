bbff4e1fcb60b7f79e1ad490de71c3c1165f4590
kengnidoriane <dorianekengni@gmail.com>
2025-06-23 12:41:25 +0200

    feat: iniitiaalize spring boot app with dependancies

---
+++ src/test/java/com/blog/Api_Blog/ApiBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.Api_Blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
