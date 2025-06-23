ba9af9cbac859a599fd8938a937a599311608205
kengnidoriane <dorianekengni@gmail.com>
2025-06-23 15:05:36 +0200

    feat: adding article and comment model

--- src/main/java/com/blog/Api_Blog/model/Article.java
+++ src/main/java/com/blog/Api_Blog/model/Article.java
@@ -1,6 +1,30 @@
 package com.blog.Api_Blog.model;
 
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime publishedAt;
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
 }
--- src/main/java/com/blog/Api_Blog/model/Comment.java
+++ src/main/java/com/blog/Api_Blog/model/Comment.java
@@ -1,4 +1,29 @@
 package com.blog.Api_Blog.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
 }
