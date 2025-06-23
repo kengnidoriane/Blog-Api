95fdf6c9fc60b3678c6825373bc583f7b7c9dad8
kengnidoriane <dorianekengni@gmail.com>
2025-06-23 16:34:39 +0200

    feat: add comments and article controller

--- src/main/java/com/blog/Api_Blog/controller/ArticleController.java
+++ src/main/java/com/blog/Api_Blog/controller/ArticleController.java
@@ -1,4 +1,44 @@
 package com.blog.Api_Blog.controller;
 
+
+import com.blog.Api_Blog.DTO.request.ArticleRequestDTO;
+import com.blog.Api_Blog.DTO.response.ArticleResponseDTO;
+import com.blog.Api_Blog.services.ArticleService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/articles")
 public class ArticleController {
+
+    @Autowired
+    private ArticleService articleService;
+
+    @PostMapping
+    public ArticleResponseDTO createArticle(@Valid @RequestBody ArticleRequestDTO requestDTO) {
+        return articleService.createArticle(requestDTO);
+    }
+
+    @GetMapping
+    public List<ArticleResponseDTO> getAllArticles() {
+        return articleService.getAllArticles();
+    }
+
+    @GetMapping("/{id}")
+    public ArticleResponseDTO getArticleById(@PathVariable Long id){
+        return articleService.getArticleById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ArticleResponseDTO updateArticle(@PathVariable Long id, @Valid @RequestBody ArticleRequestDTO requestDTO) {
+        return articleService.updateArticle(id, requestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteArticleById(@PathVariable Long id){
+        articleService.deleteArticle(id);
+    }
 }
--- src/main/java/com/blog/Api_Blog/controller/CommentController.java
+++ src/main/java/com/blog/Api_Blog/controller/CommentController.java
@@ -1,4 +1,21 @@
 package com.blog.Api_Blog.controller;
 
+
+import com.blog.Api_Blog.DTO.request.CommentRequestDTO;
+import com.blog.Api_Blog.DTO.response.CommentResponseDTO;
+import com.blog.Api_Blog.services.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/articles/{articleId}/comments")
 public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @PostMapping
+    public CommentResponseDTO addComment(@PathVariable Long articleId, @RequestBody CommentRequestDTO commentRequestDTO){
+        return commentService.addComment(articleId, commentRequestDTO);
+    }
 }
